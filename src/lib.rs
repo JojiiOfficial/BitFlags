a8ec635243afc33411b769f2312f594d39309cd8
jojii <jojii@gmx.net>
2021-06-14 15:14:03 +0200

    add overflow check

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::ops::{BitAnd, BitOr, BitXor, Not, Shl};
+use std::{
+    fmt::Display,
+    ops::{BitAnd, BitOr, BitXor, Not, Shl},
+};
 
 #[derive(Clone, Copy, Default)]
 pub struct BitFlag<T: BitflagAble> {
@@ -7,11 +10,13 @@ pub struct BitFlag<T: BitflagAble> {
 
 pub trait BitflagAble:
     BitOr<Output = Self>
+    + PartialOrd
     + BitAnd<Output = Self>
     + BitXor<Output = Self>
     + Shl<Output = Self>
     + Not<Output = Self>
     + Default
+    + Display
     + Copy
     + PartialEq
     + From<u8>
@@ -20,11 +25,13 @@ pub trait BitflagAble:
 
 impl<U> BitflagAble for U where
     U: BitOr<Output = U>
+        + PartialOrd
         + BitAnd<Output = U>
         + BitXor<Output = U>
         + Shl<Output = U>
         + Not<Output = U>
         + Default
+        + Display
         + Copy
         + PartialEq
         + From<u8>
@@ -39,6 +46,16 @@ impl<T: BitflagAble> BitFlag<T> {
 
     /// Sets a bit at the given `pos` to `val`
     pub fn set(&mut self, pos: T, val: bool) {
+        if Self::is_overflow(pos) {
+            return;
+        }
+
+        // Check for overflow
+        let size = T::from((std::mem::size_of::<T>() * 8) as u8);
+        if pos >= size {
+            return;
+        }
+
         let mask = T::from(1_u8) << pos;
 
         if val {
@@ -50,8 +67,18 @@ impl<T: BitflagAble> BitFlag<T> {
 
     /// Gets a bit at the given [`pos`]
     pub fn get(&self, pos: T) -> bool {
+        if Self::is_overflow(pos) {
+            return false;
+        }
+
         let mask = T::from(1_u8) << pos;
         (self.val & mask) == mask
+    }
+
+    /// Returns true if [`pos`] would cause an overflow
+    pub fn is_overflow(pos: T) -> bool {
+        let size = T::from((std::mem::size_of::<T>() * 8) as u8);
+        pos >= size
     }
 
     ///  Inverts all bits in [`val`]
@@ -63,6 +90,14 @@ impl<T: BitflagAble> BitFlag<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn check_overflow() {
+        let mut bf8: BitFlag<u8> = BitFlag::new();
+        // Set won't do anything
+        bf8.set(8, true);
+        assert_eq!(bf8.get(8), false);
+    }
 
     #[test]
     fn set_all_false_u8() {
